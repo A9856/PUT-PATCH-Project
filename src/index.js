ae524920be3575b601fc1c57fdaf7e5b6e429e2e
A9856 <adilkhanofficial74@gmail.com>
2025-06-14 18:06:53 +0530

    package.json index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     <>
-        <BrowserRouter>
+        <BrowserRouter basename="/Project-PUT-PATCH">
         {/* <div className="bg-secondary">
         <h1>Hello World</h1>
         </div> */}
